09490e4eb5ebafe3d2b8597b21166abdb2a6b65c
fluffyhake <haakon.reppen@gmail.com>
2023-12-09 15:20:12 +0100

    +there we go, now for part 2

--- src/2/solution.ts
+++ src/2/solution.ts
@@ -5,28 +5,32 @@ const file = fs.readFileSync(path.resolve(__dirname, '../assets/2/puzzleInput.tx
 console.log(findAllowedGames(file))
 
 
-export function findAllowedGames(file){
+export function findAllowedGames(file) {
     // add a counter for the total of all ids of allowed games
     let allowedGamesCounter: number = 0
     for (const line of file) {
-        let totalColors: {[key: string]: number} = {"Game": 0, "red": 0, "blue": 0, "green": 0}
+        let totalColors: { [key: string]: number } = { "Game": 0, "hitError": 0}
 
-        let splitLine: string[] = line.split(/[:;,]/g)
+        let splitLine: string[] = line.split(/[:;]/g)
         for (const entry of splitLine) {
-            let data: string[] = entry.trim().split(" ")
-            if (data[0] === "Game") {
-                totalColors.Game = parseInt(data[1])
-            }else{
-                totalColors[data[1]] += parseInt(data[0])
-            }
-        
+            let data: string[] = entry.trim().split(",")
+            for (const iterator of data) {
+                let entry = iterator.trim().split(" ")
+                if (entry[0] === "Game") {
+                    totalColors.Game = parseInt(entry[1])
+                } else {
+                    let pasedInt: number = parseInt(entry[0])
+                    if ((entry[1] === "red" && pasedInt > 12) || (entry[1] === "green" && pasedInt > 13) || (entry[1] === "blue" && pasedInt > 14)) {
+                        totalColors.hitError = 1
+                    }
+                }
 
+            }
         }
-        console.log(totalColors)
-        if(totalColors.red <= 12 && totalColors.blue <= 14 && totalColors.green <= 13){
+        if(totalColors.hitError === 0){
             allowedGamesCounter += totalColors.Game
         }
+
     }
     return allowedGamesCounter
-
 }
