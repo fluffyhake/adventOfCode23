79b2711dadb27a8b5b0641f659aeec0d52978669
fluffyhake <haakon.reppen@gmail.com>
2023-12-10 20:23:31 +0100

    +part 2

--- src/2/solution.ts
+++ src/2/solution.ts
@@ -3,7 +3,7 @@ import path from "path";
 const file = fs.readFileSync(path.resolve(__dirname, '../assets/2/puzzleInput.txt'), 'utf-8').split('\n');
 
 console.log(findAllowedGames(file))
-
+console.log(minimumAmoutOfCubes(file))
 
 export function findAllowedGames(file) {
     // add a counter for the total of all ids of allowed games
@@ -33,4 +33,40 @@ export function findAllowedGames(file) {
 
     }
     return allowedGamesCounter
+}
+
+
+export function minimumAmoutOfCubes(file) {
+    // add a counter for the total of all ids of allowed games
+    let totalColors: { [key: string]: number }[] = [{ "id": 0, "red": 0, "green": 0, "blue": 0 }]
+    let counter = 0
+
+
+    for (const line of file) {
+
+        let splitLine: string[] = line.split(/[:;]/g)
+        for (const entry of splitLine) {
+            let data: string[] = entry.trim().split(",")
+            for (const iterator of data) {
+                let entry = iterator.trim().split(" ")
+                if (entry[0] === "Game") {
+                    totalColors.push({ id: parseInt(entry[1]), "red": 0, "green": 0, "blue": 0 })
+                } else {
+                    let parsedInt: number = parseInt(entry[0])
+                    if (totalColors[counter][entry[1]] < parsedInt) {
+                        totalColors[counter][entry[1]] = parsedInt
+                    }
+                }
+
+            }
+        }
+        counter = counter + 1
+    }
+    let aggregatePowers: number = 0
+    for (const entry of totalColors) {
+        aggregatePowers += entry["red"] * entry["green"] * entry["blue"]
+        
+    }
+    
+    return aggregatePowers
 }
