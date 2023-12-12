268cc1f2bec32ad6e91eb0e4039086997a985f15
fluffyhake <haakon.reppen@gmail.com>
2023-12-12 22:41:08 +0100

    +formatting and tasks for solutions

--- src/1/solution.ts
+++ src/1/solution.ts
@@ -1,80 +1,79 @@
 import * as fs from "fs";
 import path from "path";
-const file = fs.readFileSync(path.resolve(__dirname, '../assets/1/puzzleInput.txt'), 'utf-8').split('\n');
+const file = fs
+  .readFileSync(path.resolve(__dirname, "../assets/1/puzzleInput.txt"), "utf-8")
+  .split("\n");
 
-console.log(findTotal(file))
-console.log(findTotal2(file))
+console.log(findTotal(file));
+console.log(findTotal2(file));
 
-
-export function findTotal(file){
-    let totalSum: number = 0
-    for (const line of file) {
-        let numberMatches: string[] = line.match(/\d/g);
-        let firstMatch: string = ""
-        let lastMatch: string = ""
-        try {
-            firstMatch = numberMatches[0]
-            lastMatch = numberMatches[numberMatches.length - 1]
-            if (firstMatch && lastMatch) {
-                // Math in javascript is allways fun. 
-                // totalSum += parseInt(`${firstMatch}${lastMatch}`)
-                totalSum += parseInt(firstMatch + lastMatch)
-            }
-        } catch (e) {
-            console.error("Line does not contain two or more numbers: " + line)
-        }
+export function findTotal(file) {
+  let totalSum: number = 0;
+  for (const line of file) {
+    let numberMatches: string[] = line.match(/\d/g);
+    let firstMatch: string = "";
+    let lastMatch: string = "";
+    try {
+      firstMatch = numberMatches[0];
+      lastMatch = numberMatches[numberMatches.length - 1];
+      if (firstMatch && lastMatch) {
+        // Math in javascript is allways fun.
+        // totalSum += parseInt(`${firstMatch}${lastMatch}`)
+        totalSum += parseInt(firstMatch + lastMatch);
+      }
+    } catch (e) {
+      console.error("Line does not contain two or more numbers: " + line);
     }
-    return totalSum
+  }
+  return totalSum;
 }
 
-
-export function findTotal2(file){
-    let totalSum: number = 0
-    for (const line of file) {
-        // Since we need to match overlapping matches (sevenine = "seven", "nine") we cant use the .match funtion in javascript
-        let numberMatches = []
-        let match
-        const pattern = /(?:one|two|three|four|five|six|seven|eight|nine|\d)/g
-        while ((match = pattern.exec(line)) !== null){
-            numberMatches.push(match[0])
-            pattern.lastIndex = match.index + 1;
-        }
-        let firstMatch: string = ""
-        let lastMatch: string = ""
-        try {
-            firstMatch = numberMatches[0]
-            lastMatch = numberMatches[numberMatches.length - 1]
-            if (firstMatch && lastMatch) {
-                
-                // convert to string for string interpolation so "3" + "5" becomes "35" and not 8.
-                let total: string = numberCheck(firstMatch).toString() + numberCheck(lastMatch).toString()
-                totalSum += parseInt(total)
-            }
-        } catch (e) {
-            console.error("Line does not contain two or more numbers: " + line)
-        }
+export function findTotal2(file) {
+  let totalSum: number = 0;
+  for (const line of file) {
+    // Since we need to match overlapping matches (sevenine = "seven", "nine") we cant use the .match funtion in javascript
+    let numberMatches = [];
+    let match;
+    const pattern = /(?:one|two|three|four|five|six|seven|eight|nine|\d)/g;
+    while ((match = pattern.exec(line)) !== null) {
+      numberMatches.push(match[0]);
+      pattern.lastIndex = match.index + 1;
     }
-    return totalSum
-
+    let firstMatch: string = "";
+    let lastMatch: string = "";
+    try {
+      firstMatch = numberMatches[0];
+      lastMatch = numberMatches[numberMatches.length - 1];
+      if (firstMatch && lastMatch) {
+        // convert to string for string interpolation so "3" + "5" becomes "35" and not 8.
+        let total: string =
+          numberCheck(firstMatch).toString() +
+          numberCheck(lastMatch).toString();
+        totalSum += parseInt(total);
+      }
+    } catch (e) {
+      console.error("Line does not contain two or more numbers: " + line);
+    }
+  }
+  return totalSum;
 }
 
-function numberCheck(word: string){
-    return Number.isNaN(parseInt(word)) ? wordToNumber(word) : parseInt(word)
+function numberCheck(word: string) {
+  return Number.isNaN(parseInt(word)) ? wordToNumber(word) : parseInt(word);
 }
 
-function wordToNumber(word: string){
-    const numberWords = {
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9
-    };
+function wordToNumber(word: string) {
+  const numberWords = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
 
-    return numberWords[word.toLowerCase()] || NaN
-
+  return numberWords[word.toLowerCase()] || NaN;
 }
--- src/2/solution.ts
+++ src/2/solution.ts
@@ -1,72 +1,78 @@
 import * as fs from "fs";
 import path from "path";
-const file = fs.readFileSync(path.resolve(__dirname, '../assets/2/puzzleInput.txt'), 'utf-8').split('\n');
+const file = fs
+  .readFileSync(path.resolve(__dirname, "../assets/2/puzzleInput.txt"), "utf-8")
+  .split("\n");
 
-console.log(findAllowedGames(file))
-console.log(minimumAmoutOfCubes(file))
+console.log(findAllowedGames(file));
+console.log(minimumAmoutOfCubes(file));
 
 export function findAllowedGames(file) {
-    // add a counter for the total of all ids of allowed games
-    let allowedGamesCounter: number = 0
-    for (const line of file) {
-        let totalColors: { [key: string]: number } = { "Game": 0, "hitError": 0}
+  // add a counter for the total of all ids of allowed games
+  let allowedGamesCounter: number = 0;
+  for (const line of file) {
+    let totalColors: { [key: string]: number } = { Game: 0, hitError: 0 };
 
-        let splitLine: string[] = line.split(/[:;]/g)
-        for (const entry of splitLine) {
-            let data: string[] = entry.trim().split(",")
-            for (const iterator of data) {
-                let entry = iterator.trim().split(" ")
-                if (entry[0] === "Game") {
-                    totalColors.Game = parseInt(entry[1])
-                } else {
-                    let pasedInt: number = parseInt(entry[0])
-                    if ((entry[1] === "red" && pasedInt > 12) || (entry[1] === "green" && pasedInt > 13) || (entry[1] === "blue" && pasedInt > 14)) {
-                        totalColors.hitError = 1
-                    }
-                }
-
-            }
+    let splitLine: string[] = line.split(/[:;]/g);
+    for (const entry of splitLine) {
+      let data: string[] = entry.trim().split(",");
+      for (const iterator of data) {
+        let entry = iterator.trim().split(" ");
+        if (entry[0] === "Game") {
+          totalColors.Game = parseInt(entry[1]);
+        } else {
+          let pasedInt: number = parseInt(entry[0]);
+          if (
+            (entry[1] === "red" && pasedInt > 12) ||
+            (entry[1] === "green" && pasedInt > 13) ||
+            (entry[1] === "blue" && pasedInt > 14)
+          ) {
+            totalColors.hitError = 1;
+          }
         }
-        if(totalColors.hitError === 0){
-            allowedGamesCounter += totalColors.Game
-        }
-
+      }
     }
-    return allowedGamesCounter
+    if (totalColors.hitError === 0) {
+      allowedGamesCounter += totalColors.Game;
+    }
+  }
+  return allowedGamesCounter;
 }
 
-
 export function minimumAmoutOfCubes(file) {
-    // add a counter for the total of all ids of allowed games
-    let totalColors: { [key: string]: number }[] = [{ "id": 0, "red": 0, "green": 0, "blue": 0 }]
-    let counter = 0
+  // add a counter for the total of all ids of allowed games
+  let totalColors: { [key: string]: number }[] = [
+    { id: 0, red: 0, green: 0, blue: 0 },
+  ];
+  let counter = 0;
 
-
-    for (const line of file) {
-
-        let splitLine: string[] = line.split(/[:;]/g)
-        for (const entry of splitLine) {
-            let data: string[] = entry.trim().split(",")
-            for (const iterator of data) {
-                let entry = iterator.trim().split(" ")
-                if (entry[0] === "Game") {
-                    totalColors.push({ id: parseInt(entry[1]), "red": 0, "green": 0, "blue": 0 })
-                } else {
-                    let parsedInt: number = parseInt(entry[0])
-                    if (totalColors[counter][entry[1]] < parsedInt) {
-                        totalColors[counter][entry[1]] = parsedInt
-                    }
-                }
-
-            }
+  for (const line of file) {
+    let splitLine: string[] = line.split(/[:;]/g);
+    for (const entry of splitLine) {
+      let data: string[] = entry.trim().split(",");
+      for (const iterator of data) {
+        let entry = iterator.trim().split(" ");
+        if (entry[0] === "Game") {
+          totalColors.push({
+            id: parseInt(entry[1]),
+            red: 0,
+            green: 0,
+            blue: 0,
+          });
+        } else {
+          let parsedInt: number = parseInt(entry[0]);
+          if (totalColors[counter][entry[1]] < parsedInt) {
+            totalColors[counter][entry[1]] = parsedInt;
+          }
         }
-        counter = counter + 1
+      }
     }
-    let aggregatePowers: number = 0
-    for (const entry of totalColors) {
-        aggregatePowers += entry["red"] * entry["green"] * entry["blue"]
-        
-    }
-    
-    return aggregatePowers
+    counter = counter + 1;
+  }
+  let aggregatePowers: number = 0;
+  for (const entry of totalColors) {
+    aggregatePowers += entry["red"] * entry["green"] * entry["blue"];
+  }
+
+  return aggregatePowers;
 }
